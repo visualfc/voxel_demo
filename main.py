b764c2d254db527dd00b9ad9014a49b99f82a4cf
visualfc <visualfc@gmail.com>
2022-05-17 22:16:16 +0800

    add text

--- main.py
+++ main.py
@@ -2,24 +2,21 @@ from scene import Scene
 import taichi as ti
 from taichi.math import *
 
-# https://www.colinfahey.com/tetris/tetris.html
-
 scene = Scene(voxel_edges=0, exposure=2)
 scene.set_floor(-0.85, (1.0, 1.0, 1.0))
 scene.set_directional_light((1, 1, -1), 0.2, (1, 0.8, 0.6))
-
 
 @ti.func
 def block(pos, size, mat, color, boder_color):
     mx, my, mz = pos.x+size.x-1, pos.y+size.y-1, pos.z+size.z-1
     for x, y, z in ti.ndrange((pos[0], pos[0] + size[0]), (pos[1], pos[1] + size[1]), (pos[2], pos[2] + size[2])):
-        bx, by, bz = (x == pos.x or x == pos.x+size.x-1), (y == pos.y or y ==
-                                                           pos.y+size.y-1), (z == pos.z or z == pos.z+size.z-1)
+        bx = (x == pos.x or x == pos.x+size.x-1)
+        by = (y == pos.y or y == pos.y+size.y-1)
+        bz = (z == pos.z or z == pos.z+size.z-1)
         if (bx and by) or (bx and bz) or (by and bz):
             scene.set_voxel(ivec3(x, y, z), mat, boder_color)
         else:
             scene.set_voxel(ivec3(x, y, z), mat, color)
-
 
 @ti.func
 def tetris(pos, r, face, border, i):
@@ -28,7 +25,6 @@ def tetris(pos, r, face, border, i):
     block(pos+ivec3(i[6:9])*r, ivec3(r, r, r), 1, face, border)
     block(pos+ivec3(i[9:12])*r, ivec3(r, r, r), 1, face, border)
 
-
 @ti.func
 def create_block(pos, size, color, color_noise):
     for I in ti.grouped(
@@ -36,17 +32,27 @@ def create_block(pos, size, color, color_noise):
                        (pos[2], pos[2] + size[2]))):
         scene.set_voxel(I, 1, color + color_noise * ti.random())
 
-
 @ti.func
 def tetris_bottom(pos, size, r, color, boder_color):
     for x in range(size):
         for z in range(size):
             block(pos+ivec3(x*r, 0, z*r), ivec3(r, r, r), 1, color, boder_color)
 
+@ti.func
+def draw_text(pos, data, color):
+    for i in range(8):
+        if ((data[0] << i)&0x00ff) >> 7 == 1:scene.set_voxel(ivec3(pos.x+i,pos.y+7,pos.z), 1, color)
+        if ((data[1] << i)&0x00ff) >> 7 == 1:scene.set_voxel(ivec3(pos.x+i,pos.y+6,pos.z), 1, color)
+        if ((data[2] << i)&0x00ff) >> 7 == 1:scene.set_voxel(ivec3(pos.x+i,pos.y+5,pos.z), 1, color)
+        if ((data[3] << i)&0x00ff) >> 7 == 1:scene.set_voxel(ivec3(pos.x+i,pos.y+4,pos.z), 1, color)
+        if ((data[4] << i)&0x00ff) >> 7 == 1:scene.set_voxel(ivec3(pos.x+i,pos.y+3,pos.z), 1, color)
+        if ((data[5] << i)&0x00ff) >> 7 == 1:scene.set_voxel(ivec3(pos.x+i,pos.y+2,pos.z), 1, color)
+        if ((data[6] << i)&0x00ff) >> 7 == 1:scene.set_voxel(ivec3(pos.x+i,pos.y+1,pos.z), 1, color)
+        if ((data[7] << i)&0x00ff) >> 7 == 1:scene.set_voxel(ivec3(pos.x+i,pos.y,pos.z), 1, color)
 
 @ti.kernel
 def initialize_voxels():
-    # Your code here! :-)
+    # https://www.colinfahey.com/tetris/tetris.html
     red, darkred = vec3(1, 0, 0), vec3(139/255, 0, 0)
     green, darkgreen = vec3(0, 1, 0), vec3(0, 100/255, 0)
     blue, darkblue = vec3(0, 0, 1), vec3(0, 0, 139/255)
@@ -63,7 +69,17 @@ def initialize_voxels():
         0, 0, 0, 1, 0, 0, 2, 0, 0, 1, 1, 0], [0, 0, 0, 0, 0, 1, 0, 0, 2, 0, 1, 1]
     pices_t3, pices_t4, pices_t5 = [0, 0, 0, 1, 0, 0, 2, 0, 0, 1, 0, -1], [
         0, 0, 0, 1, 0, 0, 2, 0, 0, 1, -1, 0], [0, 0, 0, 0, 1, 0, 0, 2, 0, 0, 1, 1]
-
+    # draw text 3d tetris
+    xoff,yoff,zoff = -60,10,-40
+    draw_text(ivec3(xoff+12,yoff+8,zoff),[0x00,0x1C,0x22,0x02,0x1C,0x02,0x22,0x1C],darkgreen)
+    draw_text(ivec3(xoff+20,yoff+8,zoff),[0x00,0x3C,0x22,0x22,0x22,0x22,0x22,0x3C],darkgreen)
+    draw_text(ivec3(xoff,yoff,zoff),[0x00,0x3E,0x08,0x08,0x08,0x08,0x08,0x08],darkgreen)
+    draw_text(ivec3(xoff+6,yoff,zoff),[0x00,0x3E,0x20,0x20,0x3E,0x20,0x20,0x3E],darkgreen)
+    draw_text(ivec3(xoff+12,yoff,zoff),[0x00,0x3E,0x08,0x08,0x08,0x08,0x08,0x08],darkgreen)
+    draw_text(ivec3(xoff+18,yoff,zoff),[0x00,0x38,0x24,0x24,0x38,0x30,0x28,0x24],darkgreen)
+    draw_text(ivec3(xoff+24,yoff,zoff),[0x00,0x1C,0x08,0x08,0x08,0x08,0x08,0x1C],darkgreen)
+    draw_text(ivec3(xoff+30,yoff,zoff),[0x00,0x1C,0x22,0x20,0x1C,0x02,0x22,0x1C],darkgreen)
+    # draw 3d tetris
     x, y, z = -30, -45, -60
     size = 6
     create_block(ivec3(x-4, y-3, z-4), ivec3(44, 3, 44),
@@ -77,7 +93,5 @@ def initialize_voxels():
     tetris(ivec3(x+size*3, y+size*2, z), size, green, darkgreen, pices_t1)
     tetris(ivec3(x+size*3, y+size*10, z), size, yellow, darkyellow, pices_t4)
 
-
 initialize_voxels()
-
 scene.finish()
